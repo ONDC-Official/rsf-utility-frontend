7b1df4e7be75e7854e8c580ff361af7855df064c
parneet_kaur <parneet.kaur@thewitslab.com>
2025-08-06 18:43:23 +0530

    chore: rebase branch with latest dev

--- src/assets/images/svg/Configuration.tsx
+++ src/assets/images/svg/Configuration.tsx
@@ -1,4 +1,3 @@
-
 import { SVGProps } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} fill="none" {...props}>
--- src/assets/images/svg/RemoveIcon.tsx
+++ src/assets/images/svg/RemoveIcon.tsx
@@ -1,4 +1,3 @@
-
 import { SVGProps } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={66} height={16} fill="none" {...props}>
