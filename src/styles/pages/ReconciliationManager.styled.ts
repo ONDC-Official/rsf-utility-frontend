db46b1c7471851c50f7f886bcf16d489bac7ccb5
parneet_kaur <parneet.kaur@thewitslab.com>
2025-08-06 16:23:52 +0530

    feat: page u

--- src/styles/pages/ReconciliationManager.styled.ts
+++ src/styles/pages/ReconciliationManager.styled.ts
@@ -32,7 +32,6 @@ export const TableHeader = styled(Box)({
   justifyContent: 'space-between',
   alignItems: 'center',
   padding: '20px 24px',
-  borderBottom: `1px solid ${colors.border.main}`,
 })
 
 export const TableTitle = styled(Typography)({
