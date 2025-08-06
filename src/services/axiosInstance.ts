00d147573222942d97de0b6b02f0e21f0aacf3f6
Parneet Kaur <parneet.kaur@thewitslab.com>
2025-08-06 22:12:24 +0530

    removed static base url

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || 'https://fis-staging.ondc.org/rsf-utility',
+  baseURL: process.env.REACT_APP_BASE_URL,
 })
 
 export default instance
