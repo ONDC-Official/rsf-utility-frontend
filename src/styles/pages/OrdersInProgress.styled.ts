d9f2a62dc45f1cb79fb353345d7c977914250e83
parneet_kaur <parneet.kaur@thewitslab.com>
2025-08-07 13:14:34 +0530

    fix: header ui

--- src/styles/pages/OrdersInProgress.styled.ts
+++ src/styles/pages/OrdersInProgress.styled.ts
@@ -6,7 +6,6 @@ import { typography } from 'theme/typography'
 export const Container = styled(Box)({
   background: colors.background.main,
   minHeight: '100%',
-  marginTop: '50px',
 })
 
 export const Header = styled(Box)({
