29b8fbb5805051d7c44a8b3b808ecc76c14cc89f
ashishjainondc <ashish.jain@ondc.org>
2025-08-10 21:28:43 +0530

    fix: import

--- src/components/common/RequiredFieldLabel/index.tsx
+++ src/components/common/RequiredFieldLabel/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { Typography } from '@mui/material'
 import { TypographyVariant } from 'enums/typography'
 import { RequiredFieldLabelProps } from 'components/common/RequiredFieldLabel/types'
-import colors from '@theme/colors'
+import colors from 'theme/colors'
 
 const RequiredFieldLabel: FC<RequiredFieldLabelProps> = ({
   children,
